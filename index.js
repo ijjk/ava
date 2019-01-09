dab0c532cab4fd569ed75a7b70104a536a356a0b
JJ Kasper <jj@jjsweb.site>
2019-01-09 17:55:12 -0600

    Made fixes based off of tests

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 // Get name of test file
 const {filename} = module.parent;
 
+if (!filename) {
+	throw new Error("The 'ava' module can only be imported in test files")
+}
+
 // Ensure the same AVA install is loaded by the test file as by the test worker
 if (process.env.AVA_PATH && process.env.AVA_PATH !== __dirname) {
 	module.exports = require(process.env.AVA_PATH)(filename);
